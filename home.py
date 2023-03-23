6007f3b2feaedc27becbbf2af4b4240c6f87fce4
opravinnikita <you@example.com>
2023-03-23 14:02:31 +0300

    changes for v1.0

--- home.py
+++ home.py
@@ -1,26 +1,23 @@
 import streamlit as st
+import pandas as pd
+import random
+import os
+
+ten_pic_names = os.listdir('data/ten_pics/')
+df = pd.read_csv('data/results.csv')
 
 st.header('Find my pic!')
 
-query = st.text_input('Write a description of the picture', ' Two people in the photo')
+query = st.text_input('Write a description of the picture', ' Two people at the photo')
 st.button('Find!')
 
-imges = ['https://static.streamlit.io/examples/cat.jpg', 
-         'https://static.streamlit.io/examples/owl.jpg',
-         'https://static.streamlit.io/examples/dog.jpg',
-         'https://static.streamlit.io/examples/cat.jpg',
-         'https://static.streamlit.io/examples/cat.jpg', 
-         'https://static.streamlit.io/examples/owl.jpg']
+img_count = st.slider('How much pic yoou need?', 2, 6, 4, 2)
 
-row_n = int(st.slider('How much pic yoou need?', 2, 6, 4, 2) / 2)
-
+rand_ind = random.sample(range(10), img_count)
 n = 0
-for i in range(row_n):
+for i in range(int(img_count / 2)):
     for col in st.columns(2):
-        col.image(imges[n], caption='Sunrise by the mountains')
+        img_name = ten_pic_names[rand_ind[n]]
+        img_discription = df[df['image_name'] == img_name]['comment'].iloc[0]
+        col.image(f'data/ten_pics/{img_name}', caption=img_discription)
         n += 1
-
-import os
-
-directories = os.system('df -H')
-print(directories)
